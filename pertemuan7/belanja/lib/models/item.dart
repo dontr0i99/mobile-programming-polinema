8c5494211ca0a4810b57a83cddef126abb3f46d8
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-10-11 06:59:33 +0700

    Delete Wrong Folder

--- pertemuan7/belanja/lib/models/item.dart
+++
@@ -1,9 +0,0 @@
-class Item {
-  final String name;
-  final int price;
-  final String image;
-  final int stock;
-  final double rating;
-
-  Item({required this.name, required this.price, required this.image, required this.stock, required this.rating});
-}
