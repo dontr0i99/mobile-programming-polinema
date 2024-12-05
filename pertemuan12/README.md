15f24c73f0704d9be52b9d99149791acf0c07a10
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-12-05 21:43:48 +0700

    W12: Jawaban Soal 9

--- pertemuan12/README.md
+++ pertemuan12/README.md
@@ -39,7 +39,7 @@ yield* Stream.periodic(
 
 ### Soal 5
 
-> <img src="docs/soal-5.gif" alt="Output-Soal-5"/>
+> <img src="docs/soal-5.gif" alt="Output-Soal-5"/><br>
 
 - Jelaskan perbedaan menggunakan `listen` dan `await for` (langkah 9) !
     > - `listen` digunakan untuk mendengarkan stream secara asinkron dengan callback yang dipanggil setiap kali data baru diterima. Penggunaan `listen` tidak memblokir eksekusi kode setelahnya, memungkinkan aplikasi untuk terus berjalan. Ini sangat cocok untuk menangani stream yang bersifat broadcast atau ketika berbagai event perlu diproses secara reaktif, seperti data baru, error, atau selesai.<br>
@@ -50,14 +50,14 @@ yield* Stream.periodic(
 
 ### Soal 6
 
-> <img src="docs/soal-6.gif" alt="Output-Soal-6"/>
+> <img src="docs/soal-6.gif" alt="Output-Soal-6"/><br>
 
 - Jelaskan maksud kode langkah 8 dan 10 tersebut!
     > - Fungsi `addRandomNumber()` menghasilkan angka acak antara 0-9 dan mengirimkannya ke stream `numberStream` menggunakan `addNumberToSink()`. Di dalam `initState()`, objek `numberStream` diinisialisasi dan stream yang ada pada `numberStreamController` didengarkan. Setiap kali ada data baru, nilai `lastNumber` diperbarui dengan memanggil `setState()` agar tampilan UI diperbarui. Pada metode `dispose()`, `numberStreamController` ditutup untuk mencegah kebocoran memori ketika widget dihapus.
 
 ### Soal 7
 
-> <img src="docs/soal-7.gif" alt="Output-Soal-7"/>
+> <img src="docs/soal-7.gif" alt="Output-Soal-7"/><br>
 
 - Jelaskan maksud kode langkah 13 sampai 15 tersebut!
     > - Fungsi `addError()` menambahkan error ke stream, yang kemudian ditangani oleh `stream.listen()`. Ketika terjadi error, callback `.onError()` akan mengubah nilai `lastNumber` menjadi `-1`. Panggilan `numberStream.addError()` memicu error dalam stream yang ditangani oleh listener.
@@ -66,7 +66,16 @@ yield* Stream.periodic(
 
 ### Soal 8
 
-> <img src="docs/soal-8.gif" alt="Output-Soal-8"/>
+> <img src="docs/soal-8.gif" alt="Output-Soal-8"/><br>
 
 - Jelaskan maksud kode langkah 1-3 tersebut!
     > - Kode tersebut membuat `StreamTransformer` untuk memodifikasi data yang diterima dari stream. Setiap nilai yang diterima akan dikalikan dengan 10, sedangkan error akan menghasilkan nilai `-1`. Transformer diterapkan pada stream, dan hasilnya didengarkan untuk memperbarui `lastNumber`. Jika terjadi error, `lastNumber` akan diubah menjadi `-1`.
+
+## Praktikum 4: Subscribe ke stream events
+
+### Soal 9
+
+> <img src="docs/soal-9.gif" alt="Output-Soal-9"/><br>
+
+- Jelaskan maksud kode langkah 2, 6 dan 8 tersebut!
+    > - Kode tersebut mendengarkan stream untuk memperbarui nilai `lastNumber` setiap kali data baru diterima. Jika terjadi error, nilai `lastNumber` diubah menjadi `-1`, dan ketika stream selesai, mencetak pesan 'onDone was called'. Pada metode `dispose()`, langganan stream dibatalkan dengan `subscription.cancel()` untuk mencegah kebocoran memori. Fungsi `addRandomNumber()` menghasilkan angka acak dan menambahkannya ke stream jika stream masih terbuka. Jika stream sudah ditutup, nilai `lastNumber` akan diubah menjadi `-1`.
--- pertemuan12/src/stream_tryo/lib/main.dart
+++ pertemuan12/src/stream_tryo/lib/main.dart
@@ -34,6 +34,7 @@ class _StreamHomePageState extends State<StreamHomePage> {
   late StreamController numberStreamController;
   late NumberStream numberStream;
   late StreamTransformer transformer;
+  late StreamSubscription subscription;
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +51,11 @@ class _StreamHomePageState extends State<StreamHomePage> {
             Text(lastNumber.toString()),
             ElevatedButton(
               onPressed: () => addRandomNumber(),
-              child: Text('New Random Number'),
+              child: const Text('New Random Number'),
+            ),
+            ElevatedButton(
+              onPressed: () => stopStream(),
+              child: const Text('Stop Subscription'),
             ),
           ],
         ),
@@ -77,8 +82,19 @@ class _StreamHomePageState extends State<StreamHomePage> {
   void addRandomNumber() {
     Random random = Random();
     int myNum = random.nextInt(10);
-    numberStream.addNumberToSink(myNum);
+    if (!numberStreamController.isClosed) {
+      numberStream.addNumberToSink(myNum);
+    } else {
+      setState(() {
+        lastNumber = -1;
+      });
+    }
+    // numberStream.addNumberToSink(myNum);
     // numberStream.addError();
+  }
+
+  void stopStream() {
+    numberStreamController.close();
   }
 
   @override
@@ -86,6 +102,20 @@ class _StreamHomePageState extends State<StreamHomePage> {
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
+    subscription = stream.listen((evebt) {
+      setState(() {
+        lastNumber = evebt;
+      });
+    });
+    subscription.onError((error) {
+      setState(() {
+        lastNumber = -1;
+      });
+    });
+    subscription.onDone(() {
+      print('onDone was called');
+    });
+    super.initState();
     // stream.listen((event) {
     //   setState(() {
     //     lastNumber = event;
@@ -95,25 +125,24 @@ class _StreamHomePageState extends State<StreamHomePage> {
     //     lastNumber = -1;
     //   });
     // });
-    transformer = StreamTransformer<int, int>.fromHandlers(
-      handleData: (value, sink) {
-        sink.add(value * 10);
-      },
-      handleError: (error, trace, sink) {
-        sink.add(-1);
-      },
-      handleDone: (sink) => sink.close(),
-    );
-    stream.transform(transformer).listen((event) {
-      setState(() {
-        lastNumber = event;
-      });
-    }).onError((error) {
-      setState(() {
-        lastNumber = -1;
-      });
-    });
-    super.initState();
+    // transformer = StreamTransformer<int, int>.fromHandlers(
+    //   handleData: (value, sink) {
+    //     sink.add(value * 10);
+    //   },
+    //   handleError: (error, trace, sink) {
+    //     sink.add(-1);
+    //   },
+    //   handleDone: (sink) => sink.close(),
+    // );
+    // stream.transform(transformer).listen((event) {
+    //   setState(() {
+    //     lastNumber = event;
+    //   });
+    // }).onError((error) {
+    //   setState(() {
+    //     lastNumber = -1;
+    //   });
+    // });
     // super.initState();
     // colorStream = ColorStream();
     // changeColor();
@@ -121,7 +150,8 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   @override
   void dispose() {
-    numberStreamController.close();
+    subscription.cancel();
+    // numberStreamController.close();
     super.dispose();
   }
 }
